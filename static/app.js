890a3e881226d30e51cd0e01ffb49b52737ac5dc
veenoise <william.eduard123@gmail.com>
2024-04-17 03:06:35 +0800

    First commit

---
+++ static/app.js
@@ -0,0 +1,8 @@
+if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+  console.log("Let's get this party started")
+}
+
+navigator.mediaDevices.getUserMedia({video: true})
+// .then(res => {
+//   console.log(res);
+// });
